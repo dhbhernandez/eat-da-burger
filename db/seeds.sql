d98667f9071ecb9f85263194721134e1b373f220
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2020-12-10 20:38:38 -0600

    updated sql and connection

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Greese Burger');
+INSERT INTO burgers (name) VALUES ('Vegetarian Burger');
+INSERT INTO burgers (name) VALUES ('Cattle Burger');
+INSERT INTO burgers (name) VALUES ('Crocodile Burger');
+INSERT INTO burgers (name) VALUES ('Snake Burger');
+INSERT INTO burgers (name) VALUES ('Cat burger');

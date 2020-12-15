3361028ac7546efcdbda91e0b6f6c5cbd6d155c7
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2020-12-15 14:43:18 -0600

    heroku update

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,10 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(150) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(150) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
 );
 SELECT * FROM burgers;

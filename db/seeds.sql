01c47c53f6c8f6bb0cbd67fe53c5c247e5ecac49
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2020-12-15 01:34:36 -0600

    picture update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ('Greese Burger');
-INSERT INTO burgers (burger_name) VALUES ('Vegetarian Burger');
-INSERT INTO burgers (burger_name) VALUES ('Cattle Burger');
-INSERT INTO burgers (burger_name) VALUES ('Crocodile Burger');
-INSERT INTO burgers (burger_name) VALUES ('Snake Burger');
-INSERT INTO burgers (burger_name) VALUES ('Cat burger');
+INSERT INTO burgers (burger_name) VALUES ('1. Greese Burger');
+INSERT INTO burgers (burger_name) VALUES ('2. Vegetarian Burger');
+INSERT INTO burgers (burger_name) VALUES ('3. Cattle Burger');
+INSERT INTO burgers (burger_name) VALUES ('4. Crocodile Burger');
+INSERT INTO burgers (burger_name) VALUES ('5. Snake Burger');
+INSERT INTO burgers (burger_name) VALUES ('6. Cat burger');
